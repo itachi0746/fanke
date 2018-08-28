280175152cd470d24c6c2e2756222a9f093b99de
John <601473400@qq.com>
2018-08-28 18:01:16 +0800

    创建iptv项目

---
+++ cm_iptv/README.md
@@ -0,0 +1,2 @@
+# ChinaMobile IPTV EPG
+
---
+++ cm_iptv/gulpfile.js
@@ -0,0 +1,38 @@
+
+var config = {
+  // src: "src/assets", // 文件资源css,js,image等
+  // srcView: "src/View/Company2", // 文件资源html等
+  server: "src/page1", // 服务器根目录
+  target: "index.html" // 要监视(执行)的html文件
+  // dest: "dev/assets", // 生产环境文件夹
+  // destView: "dev/View/Company2" // 生产环境文件夹
+};
+
+// 一次安装
+// 用cnpm安装文件夹会很多, 文件路径太深, 导致git上传不到
+// npm install pump gulp-sass gulp-clean gulp-autoprefixer gulp-clean-css gulp-imagemin gulp-changed gulp-sourcemaps gulp-uglify run-sequence gulp-rename gulp-htmlmin gulp-postcss imagemin-pngquant gulp-cache gulp-concat del browser-sync --save-dev
+var gulp        = require('gulp'),
+
+    browserSync  = require('browser-sync').create();
+
+
+// 同步更新浏览器
+
+gulp.task('browser', function () {
+    browserSync.init({
+        files: ['**'],  // 修改HTML也刷新
+        server: {
+            baseDir: config.server,  // 设置服务器的根目录
+            index: config.target // 指定默认打开的文件
+        },
+        port: 8050  // 指定访问服务器的端口号
+    });
+    
+});
+
+
+// 只执行编译sass和同步浏览器
+// gulp.task('default', function(cb) {
+//     runSequence('mincss', cb);
+// });
+gulp.task('default', ['browser']);
